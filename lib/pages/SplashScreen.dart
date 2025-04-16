1f411a0bf10c9ce71ce939f16f2a4b906040ea7a
JeffCoplin <jeffcoplin03@gmail.com>
2025-04-16 19:04:45 -0400

    Agregando SplashScreen.dart y Assets

---
+++ lib/pages/SplashScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacementNamed('/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[700],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/defensa.png',
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Defensa Civil Dominicana',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
