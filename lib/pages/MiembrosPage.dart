327d09afd9469d16c1e439cfad2fadf12a8eb4dd
JeffCoplin <jeffcoplin03@gmail.com>
2025-04-17 13:56:31 -0400

    agregando MiembrosPage

---
+++ lib/pages/MiembrosPage.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class MiembrosPage extends StatefulWidget {
+  const MiembrosPage({Key? key}) : super(key: key);
+
+  @override
+  _MiembrosPageState createState() => _MiembrosPageState();
+}
+
+class _MiembrosPageState extends State<MiembrosPage> {
+  List miembros = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMiembros();
+  }
+
+  Future<void> fetchMiembros() async {
+    final url = Uri.parse("https://adamix.net/defensa_civil/def/miembros.php");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        miembros = data['datos'];
+      });
+    } else {
+      print("Error al cargar los miembros");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      appBar: AppBar(
+        title: const Text('Miembros'),
+        backgroundColor: Colors.orange,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: miembros.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: miembros.length,
+              itemBuilder: (context, index) {
+                final miembro = miembros[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 16), // Aumentamos espacio vertical
+                  child: Card(
+                    elevation: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 120,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(
+                                image: NetworkImage(miembro['foto']),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  miembro['nombre'],
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  miembro['cargo'],
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
